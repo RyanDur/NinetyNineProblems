6cd57aca16f1c8a3bf519595e907fa41b52191ab
RyanDur <ryan.p.durling@gmail.com>
2015-05-29 10:06:10 -0500

    sdfafa

--- src/nnproblems/Arithmetic/S99Int.scala
+++ src/nnproblems/Arithmetic/S99Int.scala
@@ -13,9 +13,21 @@ object S99Int {
   def listPrimesinRange(range: Range): List[Int] =
     (primes dropWhile (x => x < range.head) takeWhile (x => x <= range.last)).toList
 
+  def printGoldbachList(range: Range): Unit =
+    range.filter(x => x.isEven && x > 2).foreach(num => num.goldbach match {
+      case (l, r) => println(num + " = " + l + " + " + r)
+    })
+
+  def printGoldbachListLimited(range: Range, limit: Int): Unit =
+    range.filter(x => x.isEven && x > 2).foreach(num => num.goldbach match {
+      case (l, r) if l > limit && r > limit => println(num + " = " + l + " + " + r)
+      case _ =>
+    })
+
   private val primes: Stream[Int] = Stream.cons(2, Stream.from(3, 2) filter (_.isPrime))
 
   implicit class S99Int(val start: Int) {
+    def isEven: Boolean = start % 2 == 0
 
     def isPrime: Boolean = (start > 1) && (primes takeWhile (_ <= Math.sqrt(start)) forall (start % _ != 0))
 
