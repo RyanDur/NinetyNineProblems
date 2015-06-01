046d2c30696791281eee11e3957c647c17c6d88c
RyanDur <ryan.p.durling@gmail.com>
2015-05-31 20:13:38 -0500

    blah

--- src/nnproblems/Binary_Trees/Tree.scala
+++ src/nnproblems/Binary_Trees/Tree.scala
@@ -1,5 +1,39 @@
 package nnproblems.Binary_Trees
 
-trait Tree {
+sealed abstract class Tree[+T] {
+  val depth: Int
+  def isBalanced: Boolean
+}
 
+case class Node[+T](value: T, left: Tree[T], right: Tree[T]) extends Tree[T] {
+  override def toString = "T(" + value.toString + " " + left.toString + " " + right.toString + ")"
+
+  val depth: Int = 1 + math.max(left.depth, right.depth)
+
+  override def isBalanced: Boolean = math.abs(left.depth - right.depth) <= 1
+}
+
+case object End extends Tree[Nothing] {
+  override def toString = "."
+
+  val depth: Int = 0
+
+  override def isBalanced: Boolean = true
+}
+
+object Node {
+  def apply[T](value: T): Node[T] = Node(value, End, End)
+}
+
+object Tree {
+  def cBalanced[A](int: Int, value: A): List[Tree[A]] = int match {
+    case n if n < 1 => List(End)
+    case n if n % 2 == 1 =>
+      val subtrees = cBalanced(n / 2, value)
+      subtrees.flatMap(l => subtrees.map(r => Node(value, l, r)))
+    case n if n % 2 == 0 =>
+      val lesserSubtrees = cBalanced((n - 1) / 2, value)
+      val greaterSubtrees = cBalanced((n - 1) / 2 + 1, value)
+      lesserSubtrees.flatMap(l => greaterSubtrees.flatMap(g => List(Node(value, l, g), Node(value, g, l))))
+  }
 }
--- src/nnproblems/Logic_and_Codes/p50.scala
+++ src/nnproblems/Logic_and_Codes/p50.scala
@@ -15,20 +15,21 @@ package nnproblems.Logic_and_Codes
 object p50 extends App {
   val a = List(("a", 45), ("b", 13), ("c", 12), ("d", 16), ("e", 9), ("f", 5)).sortWith(_._2 < _._2)
 
-  def treeIt(list: List[(Any, Int)]): List[(Any, Int)] = list match {
-    case Nil => Nil
-    case h :: Nil => list
-    case h :: h2 :: tl => treeIt(((((0, h._1), (1, h2._1)), h._2 + h2._2) :: tl).sortWith(_._2 < _._2))
-  }
-
-
-  def huffIt(acc: String, t: (Any, Any)): List[(String, String)] = (acc, t) match {
-    case (ac, (l: ((_, _), (_, _)), r: Int)) => huffIt(ac, l._1) ::: huffIt(ac, l._2)
-    case (ac, (l: Int, r: ((_, _), (_, _)))) => huffIt(ac + l, r._1) ::: huffIt(ac + l, r._2)
-    case (ac, (l: Int, r: String)) => (ac + l, r) :: Nil
-  }
-
-  val t2: (Any, Any) = treeIt(a).head
-
-  println(huffIt("", t2).sortBy(_._2))
+  // TODO works but produces warnings. needs to be redone
+//  def treeIt(list: List[(Any, Int)]): List[(Any, Int)] = list match {
+//    case Nil => Nil
+//    case h :: Nil => list
+//    case h :: h2 :: tl => treeIt(((((0, h._1), (1, h2._1)), h._2 + h2._2) :: tl).sortWith(_._2 < _._2))
+//  }
+//
+//
+//  def huffIt(acc: String, t: (Any, Any)): List[(String, String)] = (acc, t) match {
+//    case (ac, (l: ((_, _), (_, _)), r: Int)) => huffIt(ac, l._1) ::: huffIt(ac, l._2)
+//    case (ac, (l: Int, r: ((_, _), (_, _)))) => huffIt(ac + l, r._1) ::: huffIt(ac + l, r._2)
+//    case (ac, (l: Int, r: String)) => (ac + l, r) :: Nil
+//  }
+//
+//  val t2: (Any, Any) = treeIt(a).head
+//
+//  println(huffIt("", t2).sortBy(_._2))
 }
