cfd4e21f21af33dae31e449cfd4959b7ad6102e2
RyanDur <ryan.p.durling@gmail.com>
2015-06-04 16:57:30 -0500

    redo huffman

--- src/nnproblems/sec3_Logic_and_Codes/p50.scala
+++ src/nnproblems/sec3_Logic_and_Codes/p50.scala
@@ -1,5 +1,7 @@
 package nnproblems.sec3_Logic_and_Codes
 
+import scala.annotation.tailrec
+
 // Huffman code.
 // First of all, consult a good book on discrete mathematics or
 // algorithms for a detailed description of Huffman codes!
@@ -12,8 +14,39 @@ package nnproblems.sec3_Logic_and_Codes
 // huffman(List(("a", 45), ("b", 13), ("c", 12), ("d", 16), ("e", 9), ("f", 5)))
 // res0: List[String, String] = List((a,0), (b,101), (c,100), (d,111), (e,1101), (f,1100))
 
-object p50 extends App {
-  val a = List(("a", 45), ("b", 13), ("c", 12), ("d", 16), ("e", 9), ("f", 5)).sortWith(_._2 < _._2)
 
-  // TODO works but produces warnings. needs to be redone
+object p50 extends App {
+
+  sealed trait Huffman[+T] {
+    def value: Int
+
+    def list: List[(String, String)]
+  }
+
+  case class Leaf[T](left: Huffman[T], symbol: String, value: Int, right: Huffman[T]) extends Huffman[T] {
+    val list: List[(String, String)] = (left, right) match {
+      case (LeafEnd, LeafEnd) => (symbol, "") :: Nil
+      case (l, r) => l.list.map(x => (x._1, "0" + x._2)) ::: r.list.map(x => (x._1, "1" + x._2))
+    }
+  }
+
+  case object LeafEnd extends Huffman[Nothing] {
+    val value: Int = 0
+    val list: List[(String, String)] = Nil
+  }
+
+  object Leaf {
+    def apply[T](left: Huffman[T], value: Int, right: Huffman[T]): Huffman[T] = Leaf(left, "", value, right)
+  }
+
+  def huffman[T](list: List[(String, Int)]): List[(String, String)] = {
+    @tailrec def huffIt(tree: List[Huffman[T]]): Huffman[T] = tree match {
+      case Nil => LeafEnd
+      case h :: Nil => h
+      case h :: h2 :: tl => huffIt((Leaf(h, h.value + h2.value, h2) :: tl).sortBy(_.value))
+    }
+    huffIt(list.map(t => Leaf(LeafEnd, t._1, t._2, LeafEnd)).sortBy(_.value)).list
+  }
+
+  println(huffman(List(("a", 45), ("b", 13), ("c", 12), ("d", 16), ("e", 9), ("f", 5))).sortBy(_._1))
 }
